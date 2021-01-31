32b64d7f50a1cf74397b5ec0b6016601e331e542
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-01-30 19:11:33 -0600

    making login features into components and adding ternary and && operators to my app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Login from "./Login";
+
+const isLoggedin = false;
+
+const currentTime = new Date().getHours();
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {currentTime > 12 && <h1>Why are you still working</h1>}
+      {isLoggedin ? <h1>Hello</h1> : <Login />}
     </div>
   );
 }
